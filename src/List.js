3f2647dc3f4b58ecd5860fc28deccbbe187eed12
antaresvalle <antaresvll561@gmail.com>
2018-08-27 10:12:09 -0500

    task checked

--- src/List.js
+++ src/List.js
@@ -8,27 +8,25 @@ class List extends React.Component{
         super(props);
 
         this.state = {
-            tasks : [
-                    // {id: '1', text: 'Example Task 1', check:true},
-                    // {id: '2', text: 'Example Task 2', check:false},
-                    // {id: '3', text: 'Example Task 3', check:true}
-                ]
+            tasks : []
         }
 
         // Tasks and AddForm events
         this.handleAddTask = this.handleAddTask.bind(this);
+        this.handleCheck = this.handleCheck.bind(this);
 
         // Firebase events
         this.handleChildAdded = this.handleChildAdded.bind(this);
+        this.handleChildChanged = this.handleChildChanged.bind(this);
 
         // Reference for user
-
         const db = firebase.database();
         this.tasksRef = db.ref().child(`tasks/${this.props.user.uid}`);
     }
 
     componentDidMount(){
         this.tasksRef.on('child_added', this.handleChildAdded);
+        this.tasksRef.on('child_changed', this.handleChildChanged);
     }
 
     handleChildAdded(data){
@@ -38,16 +36,38 @@ class List extends React.Component{
         this.setState({tasks: newTasks});
     }
    
+    handleChildChanged(data){
+        // aqui debemos actualizar el estado con el nodo que cambio desde firebase, reemplazando valor del check 
+        console.log(data.val());
+        const taskVal = data.val();
+        taskVal.id = data.key;
+        console.log(taskVal.id);
+
+        const newtTasksList = this.state.tasks.concat([]); 
+        const taskIndex = newtTasksList.findIndex(task=>task.id === data.key);
+
+        // insert the new task in place 
+        newtTasksList.splice(taskIndex, 1, taskVal);
+        // rewrite the aray
+        this.setState({tasks: newtTasksList});
+    }
+
     handleAddTask(text){
         if(!text.length){
             return;
         }
         const key = this.tasksRef.push().key;
-        this.tasksRef.child(key).set({
+        this.taskRef.child(key).set({
             text: text,
             done: false,
         });
     }  
+
+    handleCheck(e){
+        const parent = e.target.closest('.task');
+        const tasksRef = this.tasksRef.child(parent.id);
+        tasksRef.update({done: e.target.checked});
+    }
     // 
     render(){
         return (
@@ -74,7 +94,7 @@ class List extends React.Component{
                 <section>
                     <AddForm onAdd={this.handleAddTask} />
                     <h4>Tasks</h4>
-                    <TaskList tasks={this.state.tasks}/>
+                    <TaskList tasks={this.state.tasks} onCheck={this.handleCheck}/>
                 </section>
              </section>
             )
@@ -95,7 +115,7 @@ class AddForm extends React.Component{
     }
 
     handleChange(e){
-        this.setState({value:e.target.value});
+        // this.setState({value:e.target.value});
     }
     handleClick(){
         this.props.onAdd(this.state.value); //
@@ -122,22 +142,30 @@ function TaskList(props) {
     return (
         <ul>
         {props.tasks.map(task => (
-            <Task key={task.id} text={task.text} check={task.check}/> 
+            <Task key={task.id} text={task.text} id={task.id} done={task.done} onCheck={props.onCheck} /> 
         ))}
         </ul>);
 }
 
-function Task(props){
-    return (
-        <div className="form-check">
-            <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" checked={props.check}/>
-            <label className="form-check-label" for="defaultCheck1">
-                <li key={props.id}>{props.text}</li>
-            </label>
-        </div>
-    );
+class Task extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: '',
+        }
+    }
+
+    render(){
+        return (
+            <div className="form-check task" id={this.props.id}>
+                <input className="form-check-input" type="checkbox" checked={this.props.done} onChange={this.props.onCheck}/>
+                <label className="form-check-label" for="defaultCheck1">
+                    <li key={this.props.id}>{this.props.text}</li>
+                </label>
+                <button>delete</button>
+            </div>
+        );
+    }
 }
-
-
 
 export default List;
